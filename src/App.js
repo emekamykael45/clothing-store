44b72798ecd9c6f5ac8226f83c8ec3dc72f27fea
emekamykael45 <emekamykael45@gmail.com>
2020-11-18 13:59:19 +0100

    installed redux-saga, moved user authentication to its own user-saga file, modified user-actions file in redux, and did a check for empty collections in collection and collection-overview components

--- src/App.js
+++ src/App.js
@@ -20,6 +20,7 @@ class App extends React.Component {
   unsubscribeFromAuth = null;
 
   componentDidMount() {
+    console.log("");
     // this.unsubscribeFromAuth = auth.onAuthStateChanged(async (userAuth) => {
     //   if (userAuth) {
     //     const userRef = await createUserProfileDocument(userAuth);
