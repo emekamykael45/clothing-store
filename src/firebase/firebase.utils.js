0e97594cb3300701e558823267bf22b55a79ff4a
emekamykael45 <emekamykael45@gmail.com>
2020-11-25 03:49:56 +0100

    leveraged context API in place of redux

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -1,15 +1,16 @@
-import firebase from 'firebase/app';
-import 'firebase/firestore';
-import 'firebase/auth';
+import firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/auth";
 
 const config = {
-  apiKey: 'AIzaSyCdHT-AYHXjF7wOrfAchX4PIm3cSj5tn14',
-  authDomain: 'crwn-db.firebaseapp.com',
-  databaseURL: 'https://crwn-db.firebaseio.com',
-  projectId: 'crwn-db',
-  storageBucket: 'crwn-db.appspot.com',
-  messagingSenderId: '850995411664',
-  appId: '1:850995411664:web:7ddc01d597846f65'
+  apiKey: "AIzaSyAE2KX_DVH2qRbasr-gdyZfKsQ1d99ItIo",
+  authDomain: "crown-db-c002c.firebaseapp.com",
+  databaseURL: "https://crown-db-c002c.firebaseio.com",
+  projectId: "crown-db-c002c",
+  storageBucket: "crown-db-c002c.appspot.com",
+  messagingSenderId: "4715547834",
+  appId: "1:4715547834:web:893126134b2c0bd4228683",
+  measurementId: "G-LHZEBT9JYX",
 };
 
 firebase.initializeApp(config);
@@ -29,10 +30,10 @@ export const createUserProfileDocument = async (userAuth, additionalData) => {
         displayName,
         email,
         createdAt,
-        ...additionalData
+        ...additionalData,
       });
     } catch (error) {
-      console.log('error creating user', error.message);
+      console.log("error creating user", error.message);
     }
   }
 
@@ -43,7 +44,7 @@ export const auth = firebase.auth();
 export const firestore = firebase.firestore();
 
 const provider = new firebase.auth.GoogleAuthProvider();
-provider.setCustomParameters({ prompt: 'select_account' });
+provider.setCustomParameters({ prompt: "select_account" });
 export const signInWithGoogle = () => auth.signInWithPopup(provider);
 
 export default firebase;
---
+++ src/providers/cart/cart.provider.jsx
@@ -0,0 +1,60 @@
+import React, { createContext, useState, useEffect } from "react";
+
+import {
+  addItemToCart,
+  removeItemFromCart,
+  filterItemFromCart,
+  getCartItems,
+  getCartItemsCount,
+  getCartTotal,
+} from "./cart.utils";
+
+export const CartContext = createContext({
+  hidden: true,
+  toggleHidden: () => {},
+  cartItems: [],
+  addItem: () => {},
+  removeItem: () => {},
+  clearItemFromCart: () => {},
+  cartItemsCount: 0,
+  cartTotal: 0,
+});
+
+const CartProvider = ({ children }) => {
+  const [hidden, setHidden] = useState(true);
+  const [cartItems, setCartItems] = useState(getCartItems);
+  const [cartItemsCount, setCartItemsCount] = useState(0);
+  const [cartTotal, setCartTotal] = useState(0);
+
+  const addItem = (item) => setCartItems(addItemToCart(cartItems, item));
+  const removeItem = (item) =>
+    setCartItems(removeItemFromCart(cartItems, item));
+  const toggleHidden = () => setHidden(!hidden);
+  const clearItemFromCart = (item) =>
+    setCartItems(filterItemFromCart(cartItems, item));
+
+  useEffect(() => {
+    setCartItemsCount(getCartItemsCount(cartItems));
+    setCartTotal(getCartTotal(cartItems));
+    localStorage.setItem("cartItems", JSON.stringify(cartItems));
+  }, [cartItems]);
+
+  return (
+    <CartContext.Provider
+      value={{
+        hidden,
+        toggleHidden,
+        cartItems,
+        addItem,
+        removeItem,
+        clearItemFromCart,
+        cartItemsCount,
+        cartTotal,
+      }}
+    >
+      {children}
+    </CartContext.Provider>
+  );
+};
+
+export default CartProvider;
