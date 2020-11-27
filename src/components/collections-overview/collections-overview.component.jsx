a95ae68c64198c0229edbf47e3b87dbf2273295d
emekamykael45 <emekamykael45@gmail.com>
2020-11-27 12:46:25 +0100

    leveraged apollo graph-ql in place of redux

--- src/components/collections-overview/collections-overview.component.jsx
+++ src/components/collections-overview/collections-overview.component.jsx
@@ -1,23 +1,21 @@
-import React from 'react';
-import { connect } from 'react-redux';
-import { createStructuredSelector } from 'reselect';
+import React from "react";
 
-import CollectionPreview from '../collection-preview/collection-preview.component';
+import CollectionPreview from "../collection-preview/collection-preview.component";
 
-import { selectCollectionsForPreview } from '../../redux/shop/shop.selectors';
-
-import './collections-overview.styles.scss';
+import "./collections-overview.styles.scss";
 
 const CollectionsOverview = ({ collections }) => (
-  <div className='collections-overview'>
-    {collections.map(({ id, ...otherCollectionProps }) => (
-      <CollectionPreview key={id} {...otherCollectionProps} />
-    ))}
-  </div>
+  <>
+    {collections.length ? (
+      <div className="collections-overview">
+        {collections.map(({ id, ...otherCollectionProps }) => (
+          <CollectionPreview key={id} {...otherCollectionProps} />
+        ))}
+      </div>
+    ) : (
+      <h1 style={{ textAlign: "center" }}>Nothing To Show</h1>
+    )}
+  </>
 );
 
-const mapStateToProps = createStructuredSelector({
-  collections: selectCollectionsForPreview
-});
-
-export default connect(mapStateToProps)(CollectionsOverview);
+export default CollectionsOverview;
